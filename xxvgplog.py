209ff904af475423e778e5c413c0a39553e746f2
Xiang Zhong <zhongxiang117@gmail.com>
2024-07-09 18:46:25 +0800

    xvgplot v0.4.0

--- xxvgplog.py
+++ xxvgplog.py
@@ -10,6 +10,8 @@ import argparse
 FEATURES = [
     'version 0.1.0  : plot for GMX-XVG file',
     'version 0.2.0  : powerful for continuously parse',
+    'version 0.3.0  : support plumed data',
+    'version 0.4.0  : make more versatile and calculate min/max/avg',
 ]
 
 VERSION = FEATURES[-1].split()[1]
@@ -24,11 +26,18 @@ def read_gmx_xvg(file):
     legends = []
     bad = []
     fatal = []
+    bo_plumed = False
     with open(file,'rt') as f:
         for line in f:
             l = line.strip()
-            if not len(l) or l.startswith('#'): continue
-            if l.startswith('@'):
+            if not len(l): continue
+            if l.startswith('#'):
+                if l[:2] == '#!':   # plumed data
+                    g = l[2:].split()
+                    if g[0] == 'FIELDS':
+                        bo_plumed = True
+                        legends.extend(g[1:])
+            elif l.startswith('@'):
                 l = l[1:].strip()
                 if l.startswith('legend'): continue
                 if 'legend' in l:
@@ -43,9 +52,11 @@ def read_gmx_xvg(file):
                     bad.append(l)
                 else:
                     data.append(d)
+    if not bo_plumed and legends:
+        legends.insert(0,'time')
     # check length
     if data:
-        n = len(legends)+1 if legends else len(data[0])
+        n = len(legends) if legends else len(data[0])
         get = []
         for i,d in enumerate(data):
             if len(d) != n:
@@ -60,13 +71,15 @@ def read_gmx_xvg(file):
         'x'    : [d[0] for d in data],
         'bad'  : bad,
         'fatal': fatal,
-        'size' : len(legends),
+        'size' : n,
     }
     keys = {}
     for i,e in enumerate(legends):
-        j = i + 1
-        full[e] = [d[j] for d in data]
-        keys[str(j)] = keys[e] = e
+        full[e] = t = [d[i] for d in data]
+        vmin = min(t)
+        vmax = max(t)
+        vavg = sum(t) / len(t)
+        keys[str(i)] = [e,vmin,vmax,vavg]
     full['keys'] = keys
     return full
 
@@ -110,9 +123,8 @@ plot_gmx_xvg {VERSION}
         '-g','--group',
         dest='group',
         metavar='v',
-        nargs='+',
         help=(
-            'column indexes to plot groups, first column will always be x-axis, '
+            'column indexes to plot groups, first column will be default to x-axis, '
             'thus, 1 means second column, 2 means third column. Names,case-insensitive, '
             'can also be used, abbreviation is allowed, thus `temperature` and `temp` or `te` '
             'can be the same column if unambiguously identified. Multiple groups '
@@ -161,20 +173,41 @@ plot_gmx_xvg {VERSION}
             return
 
     if w.interactive:
-        print('Note: parsing keys:')
+        print('Note: parsing keys: (index -> key  min  max  avg)')
         keys = full['keys']
         n = full['size']
-        x = None
+        ask = None
+        xaxis = 0
+        xdata = full['x']
+        outputs = []
+        for i in range(n):
+            l = '{:2}  ->  {:15}  {:15.3f}  {:15.3f}  {:15.3f}'.format(i, *keys[str(i)])
+            outputs.append(l)
         while True:
             while True:
-                if x:
-                    g = x.split()
-                    x = None    # reset
+                print('Note: to reset xaxis, input: xaxis=i')
+                if ask:
+                    g = ask.split()
+                    ask = None    # reset
                 else:
                     for i in range(n):
-                        print('  => {:} -> {:}'.format(i+1,keys[str(i+1)]))
+                        if xaxis == i:
+                            print(outputs[i] + '  (x-axis)')
+                        else:
+                            print(outputs[i])
                     t = input('Input group to be parsed (use number & space): ')
                     g = t.replace(',',' ').split()
+                    for v in g:
+                        if v.startswith('xaxis='):
+                            try:
+                                k = int(v.replace('xaxis=',' '))
+                                if k < 0 or k > n: raise ValueError
+                            except:
+                                print('  -> wrong setting xaxis, no space is allowed')
+                            else:
+                                xaxis = k
+                                xdata = full[keys[str(k)][0]]
+                                continue
                 if g:
                     try:
                         g = list(map(int,g))
@@ -182,20 +215,20 @@ plot_gmx_xvg {VERSION}
                         print('  -> wrong input: not a number: re-input')
                         continue
                     else:
-                        if any([i<=0 or i>n for i in g]):
+                        if any([i<0 or i>n for i in g]):
                             print('  -> wrong input: not in range: re-input')
                             continue
-                        select = [keys[str(v)] for v in g]
+                        select = [keys[str(v)][0] for v in g]
                         break
                 else:
                     continue
-            plot_xvg(x=full['x'],ys=[full[i] for i in select],titles=select)
+            plot_xvg(x=xdata,ys=[full[i] for i in select],titles=select)
 
-            x = input('Continue? [y|numbers]/N : ')
-            x = x.replace(',',' ')
-            if not len(x.split()) or x.strip().lower() == 'y': continue
+            ask = input('Continue? [y|numbers]/N : ')
+            ask = ask.replace(',',' ')
+            if not len(ask.split()) or ask.strip().lower() == 'y': continue
             bo = False
-            for i in x.split():
+            for i in ask.split():
                 try:
                     v = int(i)
                 except ValueError:
@@ -207,16 +240,24 @@ plot_gmx_xvg {VERSION}
         return
 
     if w.group:
+        use = list(full.keys())
+        for k in ['bad','fatal','size','keys']: use.remove(k)
         select = []
         for g in w.group.split(':'):
             p = []
             for v in g.replace(',',' ').split():
-                if v in full.keys():
-                    p.append(v)
+                if v in use or v.isdigit():
+                    if v.isdigit():
+                        if v in full['keys'].keys():
+                            p.append(full['keys'][v][0])
+                        else:
+                            print('Warning: group: unknown key <1>: {:}'.format(v))
+                    else:
+                        p.append(v)
                 else:
                     found = ok = False
                     key = None
-                    for k in full.keys():
+                    for k in use:
                         if k.startswith(v):
                             if found:
                                 print('Warning: group: duplicate key: {:}'.format(v))
@@ -229,11 +270,11 @@ plot_gmx_xvg {VERSION}
                         if ok:
                             p.append(key)
                     else:
-                        print('Warning: group: unknown key: {:}'.format(v))
+                        print('Warning: group: unknown key <2>: {:}'.format(v))
             if p:
                 select.append(p)
     else:
-        select = [[full['keys']['1']], ]
+        select = [[full['keys']['1'][0]], ]
 
     for g in select:
         plot_xvg(x=full['x'], ys=[full[i] for i in g], titles=g)
